7359a9f51175187dd45e8fcbfd74f4518462ba60
Luilex <75126378+Luilex@users.noreply.github.com>
2024-07-21 08:35:59 -0400

    Create dbrhub.lua

---
+++ dbrhub.lua
@@ -0,0 +1,12 @@
+local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/ArrayField/main/Source.lua'))()
+
+local Window = ArrayField:CreateWindow({
+   Name = "ArrayField Example Window",
+   LoadingTitle = "ArrayField Interface Suite",
+   LoadingSubtitle = "by Arrays",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "ArrayField"
+
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
